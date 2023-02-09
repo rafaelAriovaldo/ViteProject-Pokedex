e36f6d6efb62221ed297f74caf5f2b5cd77f6973
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2023-02-09 10:03:47 -0300

    update code delete routes and clean code.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,17 @@
 import express, { Response, Request } from 'express';
 import { PrismaClient } from '@prisma/client';
 
-
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-app.get('/abc', (req, res) => {
-  return res.json([
-    {
-      id: 1,
-      name: 'teste pokedex',
-    },
-    {
-      id: 2,
-      name: 'Pikaroto',
-    },
-  ]);
-});
+type RequestWithBody<BodyType> = Request<never, never, BodyType, never>;
+
 app.get('/pokemons', async (req, res) => {
   const pokemons = await prisma.pokemon.findMany();
   return res.status(200).json(pokemons);
 });
-app.get('/team/members', async (req, res) => {
+app.get('/teams', async (req, res) => {
   const team = await prisma.team.findMany({
     include: {
       pokemon: {
@@ -36,13 +25,9 @@ app.get('/team/members', async (req, res) => {
   return res.status(200).json(team);
 });
 
-app.get('/team', async (req, res) => {
-  const team = await prisma.team.findMany();
-  return res.status(200).json(team);
-});
 app.post(
   '/pokemons',
-  async (req: Request<never, never, { name: string; numberPokedex: number; img: string }, never>, res: Response) => {
+  async (req: RequestWithBody<{ name: string; numberPokedex: number; img: string }>, res: Response) => {
     const body = req.body;
     const pokemom = await prisma.pokemon.create({
       data: {
@@ -54,63 +39,52 @@ app.post(
     res.status(201).json(pokemom);
   },
 );
-app.post('/team', async (req: Request<never, never, { teamName: string }, never>, res: Response) => {
-  const body = req.body;
-  const teamName = await prisma.team.create({
+
+app.post('/teams', async (req: RequestWithBody<{ teamName: string; pokemons: number[] }>, res: Response) => {
+  const { teamName, pokemons } = req.body;
+
+  const teamT = await prisma.team.create({
     data: {
-      teamName: body.teamName,
+      teamName: teamName,
+      pokemon: {
+        connect: pokemons.map((arr) => {
+          return { id: arr };
+        }),
+      },
+    },
+    include: {
+      pokemon: true,
     },
   });
-  res.status(201).json(teamName);
-});
+  return res.status(201).json(teamT);
 
-app.post('/team/create',async (req: Request<never, never, { teamName: string; pokemons: number[] }, never>, res: Response) => {
-    const { teamName, pokemons } = req.body;
-  
-    const teamT = await prisma.team.create({
-      data: {
-        teamName: teamName,
-        pokemon: {
-          connect: pokemons.map((arr) => {
-            return { id: arr };
-          }),
-        },
-      },
-      include:{
-        pokemon:true
-      }
-    });
-    return res.status(201).json(teamT);
-    
-    /*
+  /*
     const arr = pokemons.map((poke) => {
       return {
         arr: poke,
       };
       */
-  },
-);
-app.delete(`/pokemon/:id`,  async (req, res)=>{
-const {id} = req.params
-const pokomonDeleted = await prisma.pokemon.delete({
-  where:{
-    id:Number(id)
-  }
-})
-return res.status(200).json(pokomonDeleted)
+});
+app.delete(`/pokemon/:id`, async (req, res) => {
+  const { id } = req.params;
+  const pokomonDeleted = await prisma.pokemon.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+  return res.status(200).json(pokomonDeleted);
 });
 
-app.delete(`team/:id`, async(req, res)=>{
-const {id} = req.params
-const teamDeleted = await prisma.team.delete({
-  where:{
-    id:Number(id),
-  },
+app.delete(`/teams/:id`, async (req, res) => {
+  const { id } = req.params;
+  const teamDeleted = await prisma.team.delete({
+    where: {
+      id: Number(id),
+    },
 
-//not implemented
- 
-})
-return res.status(200).json(teamDeleted)
-})
+    //not implemented
+  });
+  return res.status(200).json(teamDeleted);
+});
 
 app.listen(3000);
